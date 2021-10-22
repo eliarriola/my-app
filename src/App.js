c721fd91818e38baae47a721dbe79582afed8e86
Elizabeth <eli.arriola23@gmail.com>
2021-10-22 14:07:52 -0300

    cambios en App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 
-function Helloworld() {
+function Helloworld(props) {
   return (
-    <div id='hello'>Hello World</div>
+    <div id='hello'>
+      <h3>{props.subtitle}</h3>
+      {props.mytext}
+    </div>
   )
 }
 
 function App() {
   return (
-    <div>This is my component: <Helloworld/></div>
+    <div>
+      This is my component: 
+      <Helloworld mytext="Hola Eli" subtitle="Lorem ipsum" /> 
+      <Helloworld mytext="hola mundo" subtitle="component two"/> 
+      <Helloworld mytext="hello!" subtitle="component three" /> 
+    </div>
   );
 }
 
